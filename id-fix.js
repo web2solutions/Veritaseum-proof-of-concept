860e456118b910b6fde33d56ab928ef8031bc81f
web2solutions <eduardo@web2solutions.com.br>
2015-09-30 15:55:04 -0300

    code review

--- id-fix.js
+++ id-fix.js
@@ -5,14 +5,17 @@
   hhaW4oKnppcChzWy0yOjotMl0sIHNbOjotMl0pKSk=
 \*======================================================================*/
 
+
+
 // you may simple check if NAMESPACE is defined insted checking if it is null (throwing a reference error)
 if (	typeof (NAMESPACE) == 'undefined'  	) {
-    NAMESPACE = {};
+    window['NAMESPACE'] = {};
+
 
     // Creates an object that allocates a new or references an
     // existing very expensive resource associated with `id`
     var resource = function (id) {
-        
+        'strict';
 		// XXX bad pratice, too many var statements XXX //
 		// Private data
         //var _all_ids = new Array();
@@ -21,8 +24,8 @@ if (	typeof (NAMESPACE) == 'undefined'  	) {
         //var _expensive_resource = null;
 		
 		
-		var _all_ids = [ ]
-			, _closed = false
+		var _closed = false
+			// , _all_ids not necessary var
 			, _id = id
 			, _expensive_resource = null
 			, persona = { }
@@ -49,24 +52,31 @@ if (	typeof (NAMESPACE) == 'undefined'  	) {
         persona.getId = getId;
 
         close = function () {
-            delete _all_ids[_id];
-            this._closed = true;
+           	// XXXX delete is for associative arrays
+			// XXXX _all_ids is not necessary on this code
+			// delete _all_ids[_id];
+            
+			// XXXXX this flag is not being used on any other place of the code
+			this._closed = true;
         }
 
         persona.close = close;
         
         // Private methods
         function _lookupOrCreateExpensiveResourceById(id) {
-            _expensive_resource = _all_ids[id];
             
-            if (_expensive_resource == null) {
+			// XXXXXX handling _all_ids is very unnecessary due it does not provide any public way to set a different value to it.
+			
+			//_expensive_resource = _all_ids[id];
+            
+            //if (_expensive_resource == null) {
                 // Just pretend for the sake of this example
                 _expensive_resource = {
                     value: "I'm a very expensive resource associated with ID " + id
                 };
 
-                _all_ids[id] = _expensive_resource;
-            }
+                //_all_ids[id] = _expensive_resource;
+            //}
             
             return _expensive_resource;
         }
@@ -78,4 +88,5 @@ if (	typeof (NAMESPACE) == 'undefined'  	) {
     }
 
     NAMESPACE.resource = resource;
+	
 }
