bdbe79db16427395afe4ebfbd8a2a098aab7f9df
web2solutions <eduardo@web2solutions.com.br>
2015-10-07 02:27:36 -0300

    grunt + qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,86 +2,55 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json')
-		
-		
 		, qunit: {
-			all: ['test/*.html']
+			all: ['test/**/*.html']
 		}
 		
+		
+		
 		, 'http-server': {
- 
 			'dev': {
-	 
-				// the server root directory 
-				root: '/Users/eduardoalmeida/apps/Veritaseum-proof-of-concept/',
-	 
-				// the server port 
+				// the server root directory  ... please change it
+				root: '/Users/eduardoalmeida/apps/Veritaseum-proof-of-concept/'
+				, // the server port 
 				// can also be written as a function, e.g. 
 				// port: function() { return 8282; } 
-				port: 8282,
-	 
-				// the host ip address 
+				port: 8282
+				, // the host ip address 
 				// If specified to, for example, "127.0.0.1" the server will 
 				// only be available on that ip. 
 				// Specify "0.0.0.0" to be available everywhere 
-				host: "0.0.0.0",
-	 
-				cache: '/Users/eduardoalmeida/apps/Veritaseum-proof-of-concept/',
-				showDir : true,
-				autoIndex: true,
-	 
-				// server default file extension 
-				ext: "html",
-	 
-				// run in parallel with other tasks 
-				runInBackground: false,//|false,
-	 
-				// specify a logger function. By default the requests are 
-				// sent to stdout. 
-				logFn: function(req, res, error) { },
-	 
-				// Proxies all requests which can't be resolved locally to the given url 
-				// Note this this will disable 'showDir' 
-				//proxy: "http://someurl.com",
-	 
-				/// Use 'https: true' for default module SSL configuration 
-				/// (default state is disabled) 
-				//https: {
-				//	cert: "cert.pem",
-				//	key : "key.pem"
-				//}
-	 
-			}
-	 
+				host: "0.0.0.0"
+				, // the server cache directory  ... please change it
+				cache: '/Users/eduardoalmeida/apps/Veritaseum-proof-of-concept/'
+				, showDir: true
+				, autoIndex: true
+				, // server default file extension 
+				ext: "html"
+				, // run in parallel with other tasks 
+				runInBackground: true
+			, }
 		}
-		
-		
-		
-		
-		/*, watch: {
-			scripts: {
-				files: [
-					'id-fix.js'
-				]
-				, tasks: [
-					//'uglify'
-					//,'jshint'
-				]
-				, options: {
-					interrupt: true
-				, }
-			}
-		}*/
-		
-		
-		
-		//my_src_files: ['foo/*.js', 'bar/*.js'],
 	});
-	
 	//grunt.loadNpmTasks('grunt-contrib-watch');
 	//grunt.loadNpmTasks('grunt-contrib-jshint');
 	//grunt.loadNpmTasks('grunt-mocha');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
+	
 	grunt.loadNpmTasks('grunt-http-server');
-	grunt.registerTask('default', [ /*'readme'*/ ]);
+	
+	grunt.task.registerTask('qunittest', 'A sample task that run one test.', function(testname) {
+            if(!!testname)
+            	grunt.config('qunit.all', ['test/' + testname + '.html']);
+            grunt.task.run('qunit:all');
+    });
+	
+	grunt.registerTask('test', ['http-server:dev', 'qunittest:t']);
+	grunt.registerTask('livetest', ['http-server:dev']);
+	
+	
+	
+	
+	
+	//grunt.registerTask('default', [ /*'readme'*/ ]);
 };
--- README.md
+++ README.md
@@ -4,12 +4,44 @@ this is just a proof of concept repository.
 
 The main goal is to indentify all bugs and bad implementations on the file id.js.
 
-The final fixed code will reside inside id-fix.js file.
+The final fixed code will reside inside ***lib/id-fix.js*** file.
 
 
 ##  Live test for id-fix.js
 
-http://www.web2solutions.com.br/Veritaseum-proof-of-concept/t.html
+If you want to run and test the code, you will need ***npm***, ***Node.js***. It will require ***grunt*** installed on the test environment.
+
+Download this repository and uncompress to a given directory, lets assume: ***/Users/YourName/apps/Veritaseum-proof-of-concept/***
+
+Open and edit the file Gruntfile.js. Look for the following lines:
+
+    root: '/Users/eduardoalmeida/apps/Veritaseum-proof-of-concept/',
+
+    cache: '/Users/eduardoalmeida/apps/Veritaseum-proof-of-concept/',
+
+Now change it to:
+
+    root: '/Users/YourName/apps/Veritaseum-proof-of-concept/',
+
+    cache: '/Users/YourName/apps/Veritaseum-proof-of-concept/',
+
+Now, on terminal, navigate to the project directory:
+
+    $ cd /Users/YourName/apps/Veritaseum-proof-of-concept/
+
+Install grunt (if you don't have it installed):
+
+    $ npm install -g grunt-cli
+
+Now start the http dev server:
+
+    $ grunt http-server:dev
+
+
+#### Grunt plugins / dependencies
+
+  * grunt-http-server ( https://www.npmjs.com/package/grunt-http-server )
+
 
 ##  Code test for id-fix.js using Qunit
 
