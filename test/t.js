5087c610d9a93270e1b9270493a2b3f959ecb5e5
web2solutions <eduardo@web2solutions.com.br>
2015-10-07 22:14:40 -0300

    nom module 0.1.1 - code review + remove white spaces

--- test/t.js
+++ test/t.js
@@ -4,12 +4,12 @@
 		var resource2 = NAMESPACE.resource('Eduardo');  // working
 	  
 	  
-	  	assert.ok( ((typeof resource == "object") && (resource !== null) && (Object.prototype.toString.call(resource) !== '[object Array]')), "Passed!" );
-		assert.ok( ((typeof resource2 == "object") && (resource2 !== null) && (Object.prototype.toString.call(resource2) !== '[object Array]')), "Passed!" );
-		assert.ok( resource.getId() === 'Eduardo', "Passed!" );
-		assert.ok( resource.getExpensiveResource().value === "I'm a very expensive resource associated with ID Eduardo", "Passed!" );
-		assert.ok( resource2.getId() === 'Eduardo', "Passed!" );
-		assert.ok( resource2.getExpensiveResource().value === "I'm a very expensive resource associated with ID Eduardo", "Passed!" );
+	  	assert.ok( ((typeof resource == "object") && (resource !== null) && (Object.prototype.toString.call(resource) !== '[object Array]')), "resource is a object" );
+		assert.ok( ((typeof resource2 == "object") && (resource2 !== null) && (Object.prototype.toString.call(resource2) !== '[object Array]')), "resource2 is a object" );
+		assert.ok( resource.getId() === 'Eduardo', "resource.getId() == Eduardo" );
+		assert.ok( resource.getExpensiveResource().value === "I'm a very expensive resource associated with ID Eduardo", "getExpensiveResource() mtaches with ID" );
+		assert.ok( resource2.getId() === 'Eduardo', "resource2.getId() == Eduardo" );
+		assert.ok( resource2.getExpensiveResource().value === "I'm a very expensive resource associated with ID Eduardo", "getExpensiveResource() mtaches with ID" );
 		assert.ok( resource.getExpensiveResource() === resource2.getExpensiveResource(), "unnecessarily reallocating resource(Eduardo)" );
 		assert.ok( JSON.stringify( resource.getExpensiveResource() ) === JSON.stringify( resource2.getExpensiveResource() ), "unnecessarily reallocating resource(Eduardo)" );	
 		assert.ok( resource.getExpensiveResource().start === resource2.getExpensiveResource().start, "unnecessarily reallocating resource(Eduardo) - checking start's property value" );
@@ -20,7 +20,7 @@
 		resource.close();
 		resource2.close();
 	
-	  	assert.ok( typeof NAMESPACE.getTotalExpensiveResourcesCreated === 'undefined', "undefined" );
+	  	assert.ok( typeof NAMESPACE.getTotalExpensiveResourcesCreated === 'undefined', "NAMESPACE.getTotalExpensiveResourcesCreated is undefined - no public access" );
 	});
 	
 	
@@ -28,11 +28,7 @@
 	QUnit.test( "resource is closed at the appropriate time - @Matt", function( assert ) {
 		var rsrc1 = NAMESPACE.resource(42);
 		var rsrc2 = NAMESPACE.resource(42);
-		console.log(rsrc1);
-		
 		rsrc1.close();
-		
-		console.log(rsrc1);
 		
 		var rsrc3 = NAMESPACE.resource(42);
 		assert.ok( rsrc2.getExpensiveResource() === rsrc3.getExpensiveResource(), "unnecessarily reallocating resource(42)" );
@@ -51,11 +47,7 @@
 	QUnit.test( "resource is closed at the appropriate time - testing multiple ids - @Eduardo", function( assert ) {
 		var rsrc1 = NAMESPACE.resource(44);
 		var rsrc2 = NAMESPACE.resource(44);
-		console.log(rsrc1);
-		
 		rsrc1.close();
-		
-		console.log(rsrc1);
 		
 		var rsrc3 = NAMESPACE.resource(44);
 		assert.ok( rsrc2.getExpensiveResource() === rsrc3.getExpensiveResource(), "unnecessarily reallocating resource(42)" );
@@ -68,11 +60,7 @@
 		
 		var rsrc1_ = NAMESPACE.resource(43);
 		var rsrc2_ = NAMESPACE.resource(43);
-		console.log(rsrc1);
-		
 		rsrc1_.close();
-		
-		console.log(rsrc1_);
 		
 		var rsrc3_ = NAMESPACE.resource(43);
 		assert.ok( rsrc2_.getExpensiveResource() === rsrc3_.getExpensiveResource(), "unnecessarily reallocating resource(42)" );
@@ -88,5 +76,6 @@
 	
 	// necessary to close all active resources to make this test to be passed
 	QUnit.test( "check total active expensive resources - @Eduardo", function( assert ) {
-		assert.ok( NAMESPACE._resources_created == 0, "no active expensive resources" );
+		assert.ok( NAMESPACE._resources_created == 0, " NAMESPACE._resources_created == 0 -> no active expensive resources" );
+		assert.ok( Object.keys( NAMESPACE._all_ids ).length == 0, "NAMESPACE._all_ids is empty" );
 	});
